828b12ad08d26838223bb3ba7af34ec232d80cb2
osmosis10 <moseslemma@gmail.com>
2026-02-20 14:44:58 -0700

    Added toggleReccomendations to hide home page content based on key value of the 'enabled' key from toggling the main on/off button

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,16 +1,48 @@
- function hideRecommendations() {
-        // Targets the main feed grid
-        const feed = document.querySelector('ytd-rich-grid-renderer');
-        if (feed) {
-            feed.style.display = 'none';
-        }
-        // Targets the sidebar recommendations on watch page
-        const sidebar = document.querySelector('#secondary-inner');
-        if (sidebar) {
-            sidebar.style.display = 'none';
-        }
-    }
 
-    // Run on load and on scroll/DOM changes
-    window.addEventListener('load', hideRecommendations);
-    document.addEventListener('scroll', hideRecommendations);
+// Toggles reccomendations based on if the primary toggle is 'on' (true) or 'off' (false)
+ function toggleRecommendations(appState) {
+        // Targets the main feed grid
+        // Check if the youtube feed class exists and the YouLimit main toggle is 'ON'
+        const feed = document.querySelector('ytd-rich-grid-renderer');
+        const sidebar = document.querySelector('#secondary-inner');
+        if (feed != null && sidebar != null) {
+            if (appState) {
+                feed.style.display = 'none';
+                sidebar.style.display = 'none';
+            }
+
+            else {
+                feed.style.display = 'block';
+                sidebar.style.display = 'block';
+            }
+        }
+}
+
+
+// Listen for changes to the chrome local storage
+chrome.storage.onChanged.addListener((changes) => {
+    if (changes.enabled) {
+        toggleRecommendations(changes.enabled.newValue);
+    }
+});
+
+
+// Listen for changes to DOM tree, if observed, calls toggleReccomendation to hide newly 
+// loaded content
+const observer = new MutationObserver(() => {
+    chrome.storage.local.get('enabled', (data) => {
+        toggleRecommendations(data.enabled);
+    });
+});
+
+
+// Start watching the page for changes
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
+
+// Runs once on initial page load
+chrome.storage.local.get('enabled', (data) => {
+    toggleRecommendations(data.enabled);
+});
